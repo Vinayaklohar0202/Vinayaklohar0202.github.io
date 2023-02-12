028f9d4e202dfab5375afb90530b5b3b596be856
Vinayaklohar0202 <87648645+Vinayaklohar0202@users.noreply.github.com>
2023-02-12 19:25:23 +0530

    contact js modified

--- js/contact.js
+++ js/contact.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function sendMail() {
 	var params = {
 	  from_name: document.getElementById("name").value,
@@ -20,27 +19,4 @@ function sendMail() {
 	  })
 	  .catch(err=>console.log(err));
   
-=======
-function sendMail() {
-	var params = {
-	  name: document.getElementById("name").value,
-	  email: document.getElementById("email").value,
-	  message: document.getElementById("message").value,
-	};
-  
-	const serviceID = "service_yiveiqr";
-	const templateID = "template_mq97tzc";
-  
-	  emailjs.send(serviceID, templateID, params)
-	  .then(res=>{
-		  document.getElementById("name").value = "";
-		  document.getElementById("email").value = "";
-		  document.getElementById("message").value = "";
-		  console.log(res);
-		  alert("Your message sent successfully!!")
-  
-	  })
-	  .catch(err=>console.log(err));
-  
->>>>>>> 2870fe552ab2467db50edce70af76cfb76d1ccec
   }
